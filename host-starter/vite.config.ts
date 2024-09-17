073e02e1d036b1f9ea29b9ab428cd5a4f40c0b89
Kamilla <KamillaKa@users.noreply.github.com>
2024-09-17 11:42:50 +0300

    vite

--- host-starter/vite.config.ts
+++ host-starter/vite.config.ts
@@ -1,12 +1,21 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // TODO: federation config
+    // federation config
+    federation({
+      name: 'juutube',
+      remotes: {
+        mediastore: 'http://localhost:3001/assets/remoteEntry.js',
+        front_and_sidebar: 'http://localhost:3002/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
   ],
   resolve: {
     alias: {
