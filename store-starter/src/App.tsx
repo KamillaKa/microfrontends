4c47b97a79b8946c760e47255e3f36398781d9ba
Kamilla <KamillaKa@users.noreply.github.com>
2024-09-17 11:03:46 +0300

    Router

--- store-starter/src/App.tsx
+++ store-starter/src/App.tsx
@@ -1,15 +1,17 @@
-// FOR TESTING PURPOSES ONLY
+import { BrowserRouter as Router, } from 'react-router-dom';
 import { MediaProvider } from './contexts/MediaContext';
 import Test from './components/Test';
 import { UserProvider } from './contexts/UserContext';
 
 const App = () => {
   return (
-    <UserProvider>
-      <MediaProvider>
-        <Test />;
-      </MediaProvider>
-    </UserProvider>
+    <Router>
+      <UserProvider>
+        <MediaProvider>
+          <Test />;
+        </MediaProvider>
+      </UserProvider>
+    </Router>
   );
 };
 
