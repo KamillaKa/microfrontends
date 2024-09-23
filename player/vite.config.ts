bf8c0eef41cc921244e11761770781eca1973fbc
Kamilla <KamillaKa@users.noreply.github.com>
2024-09-23 15:17:16 +0300

    config updated

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -1,23 +1,22 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import federation from '@originjs/vite-plugin-federation';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'player',
+      name: "player",
+      filename: "remoteEntry.js",
       remotes: {
-        mediastore: 'mediastore@http://localhost:3001/remoteEntry.js',
+        mediastore: "http://localhost:3001/assets/remoteEntry.js",
       },
-      filename: 'remoteEntry.js',
       exposes: {
-        './PlayerButton': './src/components/player/VideoButtons',
-        './Player': './src/components/player/VideoPlayer',
+        "./VideoPlayer": "./src/components/VideoPlayer",
       },
-      shared: ['react', 'react-dom', 'react-router-dom'],
+      shared: ["react", "react-dom", "react-router-dom", "react-player"],
     }),
   ],
 
@@ -25,14 +24,14 @@ export default defineConfig({
     port: 3011, // Set the desired port here
   },
   preview: {
-    port: 3006, // Set the desired port here
+    port: 3011, // Set the desired port here
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
